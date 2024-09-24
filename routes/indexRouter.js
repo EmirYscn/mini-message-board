369b7a98b8f893bb422c8d478ebd0745c6c859ec
EmirYscn <87046103+EmirYscn@users.noreply.github.com>
2024-09-24 20:23:23 +0300

    individual message page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+let idCounter = 4;
 const messages = [
   {
+    id: 1,
     avatar: "female",
     title: "Hi",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi reprehenderit ad veniam aliquam sed, alias illum voluptatibus illo fugiat nihil deleniti quidem iusto praesentium reiciendis sequi est expedita id voluptas?",
@@ -11,10 +13,19 @@ const messages = [
     added: new Date(),
   },
   {
+    id: 2,
     avatar: "male",
     title: "hey",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi reprehenderit ad veniam aliquam sed, alias illum voluptatibus illo fugiat nihil deleniti quidem iusto praesentium reiciendis sequi est expedita id voluptas?",
     user: "Charles",
+    added: new Date(),
+  },
+  {
+    id: 3,
+    avatar: "female",
+    title: "Hi twice",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi reprehenderit ad veniam aliquam sed, alias illum voluptatibus illo fugiat nihil deleniti quidem iusto praesentium reiciendis sequi est expedita id voluptas?",
+    user: "Amando",
     added: new Date(),
   },
 ];
@@ -25,9 +36,20 @@ router.get("/", (req, res) => {
 router.get("/new", (req, res) => {
   res.render("form");
 });
-
+router.get("/message/:id", (req, res) => {
+  const messageId = req.params.id * 1;
+  const message = messages.find((message) => message.id === messageId);
+  let otherMessagesByUser = messages.filter(
+    (msg) => msg.user === message.user && msg.id !== messageId
+  );
+  res.render("message", {
+    message: message,
+    otherMessagesByUser,
+  });
+});
 router.post("/new", (req, res) => {
   const form = {
+    id: idCounter++,
     avatar: "male",
     title: req.body.title,
     text: req.body.message,
