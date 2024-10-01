aa0fdde48cabb791ca7c4e2bcc0ed4d07ad0d7d4
EmirYscn <87046103+EmirYscn@users.noreply.github.com>
2024-10-01 21:58:32 +0300

    fix route clashing

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,8 +4,8 @@ const indexController = require("../controllers/indexController");
 const router = express.Router();
 
 router.get("/", indexController.getMessages);
-router.get("/:sort", indexController.getMessages);
 router.get("/new", indexController.createMessageGet);
+router.get("/:sort", indexController.getMessages);
 router.post("/new", indexController.createMessagePost);
 router.get("/new/:user", (req, res) => {
   res.render("form", { replyingTo: req.params.user || null });
